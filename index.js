e825fcbd9be643e6bef277a3f3ae6b149f4460c5
jake.c.rummel@gmail.com <jake.c.rummel@gmail.com>
2018-09-05 20:43:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemInfo = {itemName: '${item}', itemPrice: } )
+ cart.push(itemInfo)
+ return '${item} has been added to your cart.'
 }
 
 function viewCart() {
